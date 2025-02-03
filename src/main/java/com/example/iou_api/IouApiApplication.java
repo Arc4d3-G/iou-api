ae541523b5a06ce0222c87f8040c4400245b0d81
Dewald Breed <brddew8@protonmail.com>
2025-02-03 18:41:43 +0200

    initial commit

---
+++ src/main/java/com/example/iou_api/IouApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.iou_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IouApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IouApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/iou_api/IouApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iou_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IouApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
